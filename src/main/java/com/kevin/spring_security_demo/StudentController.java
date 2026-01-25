4d4cdd1886a3fdad222233c4e5abd09961aed57b
ya.nematov <nematovmuhammadqodir68@gmail.com>
2026-01-25 23:38:15 +0900

    configuration

---
+++ src/main/java/com/kevin/spring_security_demo/StudentController.java
@@ -0,0 +1,34 @@
+package com.kevin.spring_security_demo;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.web.bind.annotation.RestController;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.security.web.csrf.CsrfToken;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@RestController
+public class StudentController {
+
+    List<Student> students = new ArrayList<>(List.of(
+            new Student(1, "Kevin", "JS"), new Student(2, "Navin", "Java"), new Student(3, "Nuri", "Python")));
+
+    @GetMapping("students")
+    public List<Student> getAllStudents() {
+        return students;
+    }
+
+    @PostMapping("students")
+    public void addStudents(@RequestBody Student student) {
+        students.add(student);
+    }
+
+    @GetMapping("/csrf-token")
+    public CsrfToken getCsrfToken(HttpServletRequest request) {
+        return (CsrfToken) request.getAttribute(CsrfToken.class.getName());
+
+    }
+
+}
